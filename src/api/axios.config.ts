d9235ad7eeee021d65ad01d00e7e0d8653cc1427
Derek Vawdrey <derek.vawdrey@gmail.com>
2026-02-11 10:58:10 -0700

    Updating base config to make it easy

--- src/api/axios.config.ts
+++ src/api/axios.config.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE = import.meta.env.VITE_API_BASE ?? '/api'
+const API_BASE = import.meta.env.VITE_API_BASE ?? 'http://localhost:3000/api'
 
 export const apiClient = axios.create({
   baseURL: API_BASE,
